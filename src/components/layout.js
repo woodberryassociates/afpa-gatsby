0eec1835f08fdee12380211e15b3a2916dfa14e9
Avi Nerenberg <devavmeister@gmail.com>
2019-08-05 18:24:13 -0400

    prettier fixes

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,12 +5,12 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react'
+import PropTypes from 'prop-types'
+import { useStaticQuery, graphql } from 'gatsby'
 
-import Header from "./header"
-import "./layout.css"
+import Header from './header'
+import './layout.css'
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -38,8 +38,7 @@ const Layout = ({ children }) => {
         <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>.
-          {` `}Theme by{` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>.{` `}Theme by{` `}
           <a href="https://www.avinerenberg.com">Avi Nerenberg</a>.
         </footer>
       </div>
