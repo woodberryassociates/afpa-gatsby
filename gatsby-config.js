67d9aa5eae904ea6072871ad51ce677df1c24c5f
Avi Nerenberg <devavmeister@gmail.com>
2019-12-15 17:59:13 -0500

    new favicon; rm'ed nvmrc

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
 				background_color: `#ffffff`,
 				theme_color: `#663399`,
 				display: `minimal-ui`,
-				icon: `src/images/gatsby-icon.png`,
+				icon: `src/images/afpa.png`,
 			},
 		},
 		{
