6b0991369dbd476bfcf9d4d89f2832a107862614
Avi Nerenberg <devavmeister@gmail.com>
2020-04-13 17:44:11 -0400

    rm's COVID featured post header, debugs COVID page sourcing by replacing title filter w/ WP ID filter

--- src/pages/covid-19.tsx
+++ src/pages/covid-19.tsx
@@ -1,5 +1,5 @@
 import { graphql } from 'gatsby'
-import BackgroundImage from 'gatsby-background-image'
+import Img from 'gatsby-image'
 import React from 'react'
 
 import CovidResources from '../components/covidPage/covidResources'
@@ -9,40 +9,16 @@ import SEO from '../components/seo'
 const CovidPage = ({
 	data: {
 		page: { title, content, featured_media },
-		featured,
 	},
 }) => (
 	<Layout>
 		<SEO title="COVID-19" />
 		<div>
 			{/* COVID Header */}
-			<BackgroundImage
+			<Img
 				className="max-h-500 md:min-h-500 flex items-center imgTop"
-				fluid={[
-					featured_media.localFile.childImageSharp.fluid,
-					`linear-gradient(rgba(36, 43, 96, 0.79), rgba(36, 43, 96, 0.79))`,
-				].reverse()}
-			>
-				<div>
-					<div className="mx-2 lg:ml-64 max-w-md flex flex-col justify-between items-start">
-						<h4
-							className="text-white leading-normal"
-							dangerouslySetInnerHTML={{ __html: featured.edges[0].node.title }}
-						/>
-						<p
-							className="text-white leading-relaxed my-4 content"
-							dangerouslySetInnerHTML={
-								featured.edges[0].node.acf.author
-									? { __html: featured.edges[0].node.acf.author }
-									: { __html: `` }
-							}
-						/>
-						<a href={featured.edges[0].node.acf.textLink}>
-							<button>{featured.edges[0].node.acf.link_text}</button>
-						</a>
-					</div>
-				</div>
-			</BackgroundImage>
+				fluid={featured_media.localFile.childImageSharp.fluid}
+			/>
 
 			<div className="mt-2 lg:mt-10 mb-10 mx-2 md:mx-32 xl:mx-64 pb-10">
 				<h4 dangerouslySetInnerHTML={{ __html: title }} />
@@ -58,7 +34,7 @@ const CovidPage = ({
 
 export const pageQuery = graphql`
 	query CovidPageQuery {
-		page: wordpressPage(title: { eq: "COVID-19" }) {
+		page: wordpressPage(wordpress_id: { eq: 768 }) {
 			id
 			title
 			content
@@ -68,19 +44,6 @@ export const pageQuery = graphql`
 						fluid(maxWidth: 1920) {
 							...GatsbyImageSharpFluid
 						}
-					}
-				}
-			}
-		}
-		featured: allWordpressWpCovid19S(limit: 1) {
-			edges {
-				node {
-					id
-					title
-					acf {
-						author
-						textLink
-						link_text
 					}
 				}
 			}
