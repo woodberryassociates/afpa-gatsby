f7305f4c4e3571431020021089d8e0b2b54d9ffa
Avi Nerenberg <devavmeister@gmail.com>
2020-05-22 18:19:34 -0400

    adds plugins dir w/ croudskout code

---
+++ src/plugins/croudSkout.js
@@ -0,0 +1,16 @@
+;(function(s, k, o, u, t) {
+	s.cs =
+		s.cs ||
+		function() {
+			cs.q.push(arguments)
+		}
+	cs.q = cs.q || []
+	cs.apiUrl = t
+	s.sourceId = 7809
+	s.clientId = 1947
+	s.organizationId = 1630046
+	var a = k.getElementsByTagName(o)[0]
+	var b = k.createElement(o)
+	b.src = u + `/analytics.js`
+	a.parentNode.insertBefore(b, a)
+})(window, document, `script`, `//s.crowdskout.com`, `https://a.crowdskout.com`)
