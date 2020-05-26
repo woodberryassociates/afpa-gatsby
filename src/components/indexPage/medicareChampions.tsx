17333bc4833bc50eebb6250b63b1c65972421924
Avi Nerenberg <devavmeister@gmail.com>
2020-05-26 13:10:32 -0400

    new padding to be consistent w/ slider button placement

--- src/components/indexPage/medicareChampions.tsx
+++ src/components/indexPage/medicareChampions.tsx
@@ -13,13 +13,15 @@ const MedicareChampions = () => (
 			`linear-gradient(rgba(36, 43, 96, 0.79), rgba(36, 43, 96, 0.79))`,
 		].reverse()}
 	>
-		<div className="h-full w-full px-2 flex flex-col justify-center items-center">
-			<div className="max-w-md mr-0 lg:mr-24 flex flex-col justify-between items-start">
+		<div className="absolute bottom-0 ml-2 mb-2 md:ml-12 md:mb-4 lg:mb-12 flex flex-col justify-center items-center">
+			<div className="flex flex-col justify-between items-start">
 				<h2 className="text-white text-2xl leading-relaxed my-2">
 					AfPA Recognizes Champions of Patient Access
 				</h2>
 				<Link to="2020-champions-of-patient-access">
-					<button>Learn more</button>
+					<button className="p-2 sm:py-3 sm:px-8 text-sm sm:text-base">
+						Learn more
+					</button>
 				</Link>
 			</div>
 		</div>
