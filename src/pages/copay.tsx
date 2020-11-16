598225c8a96d88b7f82fdbecc1be3cac00962a84
Avi Nerenberg <devavmeister@gmail.com>
2020-11-16 15:48:08 -0500

    adds copay page for copay accumulator resource custom post types

---
+++ src/pages/copay.tsx
@@ -0,0 +1,54 @@
+import { graphql } from 'gatsby'
+import Img from 'gatsby-image'
+import React from 'react'
+
+import CovidResources from '../components/covidPage/covidResources'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const CopayPage = ({
+	data: {
+		page: { title, content, featured_media },
+	},
+}) => (
+	<Layout>
+		<SEO title={title} />
+		<div>
+			{/* COVID Header */}
+			<Img
+				imgStyle={{ objectPosition: `top` }}
+				fluid={featured_media.localFile.childImageSharp.fluid}
+			/>
+
+			<div className="mt-2 lg:mt-10 mb-10 mx-2 md:mx-32 xl:mx-64 pb-10">
+				<h4 dangerouslySetInnerHTML={{ __html: title }} />
+				<div className="content flex flex-col">
+					<div dangerouslySetInnerHTML={{ __html: content }} />
+				</div>
+			</div>
+
+			<CovidResources />
+		</div>
+	</Layout>
+)
+
+export const pageQuery = graphql`
+	query CopayPageQuery {
+		page: wordpressPage(wordpress_id: { eq: 768 }) {
+			id
+			title
+			content
+			featured_media {
+				localFile {
+					childImageSharp {
+						fluid(maxWidth: 1920) {
+							...GatsbyImageSharpFluid
+						}
+					}
+				}
+			}
+		}
+	}
+`
+
+export default CopayPage
