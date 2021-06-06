7ab361e9dc77790f260a27d87d057b0b0a9e69e0
Avi Nerenberg <devavmeister@gmail.com>
2021-06-05 21:00:47 -0400

    chore(devops): integrate logrocket (not sure if it's correctly implemented)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,7 @@
-require(`typeface-montserrat`)
+import 'typeface-montserrat'
+
+import LogRocket from 'logrocket'
+
+export const onClientEntry = () => {
+	LogRocket.init(`lp8dfd/alliance-for-patient-access`)
+}
