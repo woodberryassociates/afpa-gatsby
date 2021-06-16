8ef310d3dfb51a8666d0920e3b2d6f32459b4e15
Avi Nerenberg <devavmeister@gmail.com>
2021-06-16 17:32:31 -0400

    fix(): website is only loading once & failing successive load
    Disabling LogRocketReact, which is the last thing I added (2 weeks ago).

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,9 @@
 import 'typeface-montserrat'
 
 import LogRocket from 'logrocket'
-import setupLogRocketReact from 'logrocket-react'
+// import setupLogRocketReact from 'logrocket-react'
 
 export const onClientEntry = () => {
 	LogRocket.init(`lp8dfd/alliance-for-patient-access`)
-	setupLogRocketReact(LogRocket)
+	// setupLogRocketReact(LogRocket)
 }
