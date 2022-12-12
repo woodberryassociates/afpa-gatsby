444d7331282e3b08d1956173e4318dec44f7f804
Avi Nerenberg <devavmeister@gmail.com>
2022-12-12 18:21:58 -0500

    fix(icer): corrects yt embed link for event recording

--- src/pages/icer.tsx
+++ src/pages/icer.tsx
@@ -29,7 +29,7 @@ const IcerPage = ({
 				style={{ paddingBottom: `56.25%` }}
 			>
 				<iframe
-					src="https://youtu.be/7kIsB1BwH00"
+					src="https://www.youtube.com/embed/7kIsB1BwH00"
 					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
 					allowFullScreen
 					frameBorder="0" // TODO - deprecated (does it need replacing w/ an alternative?)
